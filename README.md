2be88cc3350820c9c4c35b16be721a5313d534d1
Nathaniel Hartlen <nwhartlen@icloud.com>
2023-09-17 12:32:29 -0300

    base project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # robcoterminal
-ever since fallout 3 terminals have spawned many thoughts like why can't i do that , why can't I write things in here now you can!
+ever since fallout 3 terminals have spawned many thoughts like "why can't I do _____" , why can't I write things in here now you can!
 
 
---
+++ Source.cpp
@@ -0,0 +1,51 @@
+﻿#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <stdio.h>
+#include <cstdlib>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+int main() {
+
+	printf("robco logo does not work rn\n");
+	
+	while (true) {
+		string input;
+		string output;
+		string arg1;
+		string arg2;
+		cout << ">";
+		cin >> input;
+
+		if (input == "cls") {
+			system("cls");
+		}
+		if (input == "run") {
+			cout << "module:";
+			cin >> arg1;
+			cout << "arg:";
+			cin >> arg2;
+
+
+		}
+		if (input == "") {
+
+		}
+		if (input == "") {
+
+		}
+		if (input == "") {
+
+		}
+		if (input == "") {
+
+		}
+
+		cout << output;
+		cout << "\n";
+	}
+	return 0;
+}
